ca56327e10bee4df0a7edf0c4785b193ab8f2bff
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-07-01 08:51:21 -0500

    Fixed students appearing twice

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,5 +1,10 @@
-var students = { };
-var levels = { };
+var students = {
+  photos : { },
+  displayNames: { }
+};
+var gamedata = {
+  levels : { },
+};
 
 var iconTemplate = $("<div></div>");
 
@@ -21,22 +26,31 @@ function getIcon(uid) {
 }
 
 function updateName(uid, name) {
+  students.displayNames[uid] = name;
   getIcon(uid).find('[data-icon="name"]').html(name);
 }
 
 function updateThumbnail(uid, photo) {
+  students.photos[uid] = photo;
   getIcon(uid).find('[data-icon="thumbnail"]').attr('src', photo);
 }
 
 function studentClick(event) {
   var uid = $(event.currentTarget).attr('data-uid');
-  console.log(uid);
+  $("#badge").find('[data-icon="level"]').html(gamedata.levels[uid]);
+  $("#badge").find('[data-icon="name"]').html(students.displayNames[uid]);
+  $("#badge").find('[data-icon="thumbnail"]').attr('src', students.photos[uid]);
+  firebase.database().ref('/gamedata').child('by_uid').child(uid).once('value').then(function(snapshot) {
+
+    $("#badge").modal("show");
+  });
 }
 
 function updateLevel(uid, level) {
   if (!level) {
     level = "";
   }
+  gamedata.levels[uid] = level;
   getIcon(uid).find('[data-icon="level"]').html(level);
 }
 
@@ -51,8 +65,13 @@ function setupHandlers() {
     updateName(snapshot.key, snapshot.val());
   });
   firebase.database().ref('/students').child('displayNames').on('child_added', function(snapshot, prevKey) {
+    var uid = snapshot.key;
     buildStudent(snapshot.key);
     updateName(snapshot.key, snapshot.val());
+    firebase.database().ref('/gamedata').child('levels').child(uid).once('value', function(snapshot) {
+      var level = snapshot.val();
+      updateLevel(uid, level);
+    });
   });
   firebase.database().ref('/students').child('photos').on('child_changed', function(snapshot, prevKey) {
     updateThumbnail(snapshot.key, snapshot.val());
@@ -72,6 +91,7 @@ function loggedOut() {
 
 $(document).ready(function() {
   buildStudentIconTemplate();
+  $('[data-icon="studentView"]').load("elements.html #studentView");
   firebase.auth().onAuthStateChanged(function(user) {
     // Event when a Firebase user authenticates or signs out
     if (user) {
